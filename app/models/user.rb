4bc22f2d3d3a122769375f1b0337939be23a1987
ankitsahitya <ankit@gkmit.co>
2021-11-05 13:33:05 +0530

    user table added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :email, presence: true
+  validates :username, :email, :phone, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
