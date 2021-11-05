a9202b711cf18882282cf8e990a6090ef82847fb
ankitsahitya <ankit@gkmit.co>
2021-11-05 14:19:06 +0530

    event model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, :email, presence: true
-  validates :username, :email, :phone, uniqueness: true
+  validates :username, :email, uniqueness: true
+  validates :phone, uniqueness: true, allow_nil: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
